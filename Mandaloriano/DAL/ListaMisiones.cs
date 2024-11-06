43edb8d4c90c15d44153faff8dd65e4aa50cc5a3
af3d3z <alonso.fernandez@iesnervion.es>
2024-11-06 14:33:18 +0100

    Estructura

---
+++ Mandaloriano/DAL/ListaMisiones.cs
@@ -0,0 +1,23 @@
+﻿using ENT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ListaMisiones
+    {
+        private List<Mision> _listaMisiones;
+
+        public List<Mision> ListaMisiones() {
+            List<Mision> misiones = new List<Mision>();
+            misiones.Add(new Mision(1, "Rescate de Baby Yoda", "Debes hacerte con Grogu y llevárselo a Luke SkyWalker para su entrenamiento.", 5000));
+            misiones.Add(new Mision(2, "Recuperar armadura Beskar", "Tu armadura de Beskar ha sido robada. Debes encontrarla.", 2000));
+            misiones.Add(new Mision(1, "Planeta Sorgon", "Debes llevar a un niño de vuelta a su planeta natal \"Sorgon\".", 500));
+            misiones.Add(new Mision(1, "Renacuajos", "Debes llevar a una Dama Rana y sus huevos de Tatooine a la luna del estuario Trask, donde su esposo fertilizará los huevos.", 500));
+            return misiones;
+        }
+    }
+}
---
+++ Mandaloriano/ENT/Mision.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class Mision
+    {
+        #region atributos
+        private int _id;
+        private string _titulo;
+        private string _descripcion;
+        private int _recompensa;
+        #endregion
+
+        #region propiedades
+        public int ID {
+            get { return _id; }
+        }
+
+        public string Titulo { 
+            get { return _titulo; }
+            set { _titulo = value; }
+        }
+
+        public string Descripcion { 
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        public int Recompensa { 
+            get { return _recompensa; }
+            set { _recompensa = value; }
+        }
+
+        #endregion
+
+        #region constructores
+        public Mision(int id, string titulo, string desc, int recompensa) { 
+            this._id = id;
+            this._titulo = titulo;
+            this._descripcion = desc;
+            this._recompensa = recompensa;
+        }
+        #endregion
+    }
+}
